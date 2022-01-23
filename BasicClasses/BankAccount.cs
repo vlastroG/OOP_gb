9c5e55e432e3644da3023d9e735ee95377ba2483
Vladimir <vlastroganov@gmail.com>
2022-01-23 13:48:19 +0300

    №1

---
+++ BasicClasses/BankAccount.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClasses
+{
+    public enum BankAccountType
+    {
+        Current,
+        Settlement,
+        Credit,
+        Deposit
+        //текущий       -   current;
+        //расчетный     -   settlement;
+        //кредитный     -   credit;
+        //депозитный    -   deposit.
+    }
+    internal class BankAccount
+    {
+        private int _Number;
+        private double _Balance;
+        private BankAccountType _Type;
+        public int GetNumber()
+        {
+            return _Number;
+        }
+        public void SetNumber(int newNumber)
+        {
+            if (newNumber >= 1)
+            {
+                _Number = newNumber;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(newNumber));
+            }
+        }
+        public double GetBalance()
+        {
+            return _Balance;
+        }
+        public void SetBalance(double newBalance)
+        {
+            _Balance = newBalance;
+        }
+        public BankAccountType GetType()
+        {
+            return _Type;
+        }
+        public void SetType(BankAccountType newType)
+        {
+            _Type = newType;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Номер счета: {_Number};\tБаланс: {_Balance};\tТип: {_Type}");
+        }
+        public BankAccount()
+        {
+        }
+        public BankAccount(int number, double balance, BankAccountType type)
+        {
+            SetNumber(number);
+            SetBalance(balance);
+            SetType(type);
+        }
+    }
+
+}
--- BasicClasses/Program.cs
+++ BasicClasses/Program.cs
@@ -6,7 +6,14 @@ namespace BasicClasses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Создать класс счет в банке с закрытыми полями:
+            //номер счета, баланс, тип банковского счета(использовать перечислимый тип).
+            //Предусмотреть методы для доступа к данным – заполнения и чтения.Создать объект класса,
+            //заполнить его поля и вывести информацию об объекте класса на печать.
+            BankAccount acc1 = new BankAccount(1,1000,BankAccountType.Deposit);
+            acc1.PrintInfo();
+            
         }
+
     }
 }
