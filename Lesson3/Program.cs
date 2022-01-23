e9fe2cfddb4733303dbb8f9ccb1c75545f253453
Vladimir <vlastroganov@gmail.com>
2022-01-23 16:44:22 +0300

    №1

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -6,7 +6,23 @@ namespace Lesson3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //1. В класс банковский счет, созданный в упражнениях, добавить метод,
+            //который переводит деньги с одного счета на другой.
+            //У метода два параметра:
+            //ссылка на объект класса банковский счет откуда снимаются деньги,
+            //второй параметр – сумма.
+            Console.WriteLine("№1");
+            BankAccount acc1 = new(1000);
+            acc1.PrintInfo();
+            BankAccount acc2 = new();
+            acc2.PrintInfo();
+            acc2.RequestMoney(acc2, 1000);
+            acc2.RequestMoney(acc1, 2000);
+            acc2.RequestMoney(acc1, -50);
+            acc2.RequestMoney(acc1, 500);
+            acc1.PrintInfo();
+            acc2.PrintInfo();
+            Console.WriteLine();
         }
     }
 }
