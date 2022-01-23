8b41601391905c0eee7622345d2ad4d1f6fff47e
Vladimir <vlastroganov@gmail.com>
2022-01-23 13:56:16 +0300

    Добавление комментариев к №1

--- BasicClasses/BankAccount.cs
+++ BasicClasses/BankAccount.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace BasicClasses
 {
+    /// <summary>
+    /// Тип банковского аккаунта
+    /// </summary>
     public enum BankAccountType
     {
         Current,
@@ -17,15 +20,36 @@ namespace BasicClasses
         //кредитный     -   credit;
         //депозитный    -   deposit.
     }
+    /// <summary>
+    /// Банковский счет
+    /// </summary>
     internal class BankAccount
     {
+        /// <summary>
+        /// Номер счета
+        /// </summary>
         private int _Number;
+        /// <summary>
+        /// Баланс счета
+        /// </summary>
         private double _Balance;
+        /// <summary>
+        /// Тип счета
+        /// </summary>
         private BankAccountType _Type;
+        /// <summary>
+        /// Получить номер счета
+        /// </summary>
+        /// <returns>Номер счета</returns>
         public int GetNumber()
         {
             return _Number;
         }
+        /// <summary>
+        /// Установить номер счета
+        /// </summary>
+        /// <param name="newNumber">Новый номер счета</param>
+        /// <exception cref="ArgumentOutOfRangeException">Неверный номер счета</exception>
         public void SetNumber(int newNumber)
         {
             if (newNumber >= 1)
@@ -37,29 +61,57 @@ namespace BasicClasses
                 throw new ArgumentOutOfRangeException(nameof(newNumber));
             }
         }
+        /// <summary>
+        /// Получить баланс
+        /// </summary>
+        /// <returns>Баланс</returns>
         public double GetBalance()
         {
             return _Balance;
         }
+        /// <summary>
+        /// Установить новый баланс
+        /// </summary>
+        /// <param name="newBalance">Новый баланс</param>
         public void SetBalance(double newBalance)
         {
             _Balance = newBalance;
         }
+        /// <summary>
+        /// Получить тип счета
+        /// </summary>
+        /// <returns>Тип счета</returns>
         public BankAccountType GetType()
         {
             return _Type;
         }
+        /// <summary>
+        /// Установить новый тип счета
+        /// </summary>
+        /// <param name="newType">Новый тип счета</param>
         public void SetType(BankAccountType newType)
         {
             _Type = newType;
         }
+        /// <summary>
+        /// Вывести информацию о счете в консоль: номер, баланс, тип
+        /// </summary>
         public void PrintInfo()
         {
             Console.WriteLine($"Номер счета: {_Number};\tБаланс: {_Balance};\tТип: {_Type}");
         }
+        /// <summary>
+        /// Переопределение стандартного конструктора
+        /// </summary>
         public BankAccount()
         {
         }
+        /// <summary>
+        /// Полный конструктор
+        /// </summary>
+        /// <param name="number">Номер счета</param>
+        /// <param name="balance">Баланс</param>
+        /// <param name="type">Тип счета</param>
         public BankAccount(int number, double balance, BankAccountType type)
         {
             SetNumber(number);
