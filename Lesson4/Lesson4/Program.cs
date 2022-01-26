596026a59432600def8cdff3a95496f19f8b9d6e
Vladimir <vlastroganov@gmail.com>
2022-01-26 23:08:32 +0300

    №2

--- Lesson4/Lesson4/Program.cs
+++ Lesson4/Lesson4/Program.cs
@@ -6,12 +6,13 @@ namespace Lesson4
     {
         static void Main(string[] args)
         {
-            Building b1 = new();
+            Creator.Building b1 = new();
             b1.PrintInfo();
 
-            Building b2 = new(8, 25, 7);
+            Creator.Building b2 = new(8, 25, 7);
             b2.PrintInfo();
 
+            Console.WriteLine("№1");
             Console.WriteLine();
             Console.WriteLine($"b1 FloorHeight: {b1.FloorHeight}");
             Console.WriteLine($"b2 FloorHeight: {b2.FloorHeight}");
@@ -28,6 +29,13 @@ namespace Lesson4
             Console.WriteLine($"b1 Levels: {b1.Levels}");
             Console.WriteLine($"b2 Levels: {b2.Levels}");
             Console.WriteLine($"b2 & b1 Numbers are: {b2.Number} & {b1.Number}");
+            Console.WriteLine();
+
+            Console.WriteLine("№2");
+            Console.WriteLine(Creator.HashTable.Count);
+            Creator.HashTable.Remove(b2.Number);
+            Console.WriteLine(Creator.HashTable.Count);
+            Console.WriteLine();
         }
     }
 }
